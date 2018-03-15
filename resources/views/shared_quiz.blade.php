910a6d4b6129b9cdd30acb1d9281af6b97144729
kimkundad <kim.kundad@gmail.com>
2018-03-15 13:35:13 +0700

    golf

--- resources/views/shared_quiz.blade.php
+++ resources/views/shared_quiz.blade.php
@@ -17,9 +17,12 @@
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
     js = d.createElement(s); js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1";
+    js.src = "http://connect.facebook.net/en_US/sdk.js#xfbml=1";
     fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));</script>
+  }(document, 'script', 'facebook-jssdk'));
+
+
+</script>
 
   <!-- Your share button code -->
   <div class="fb-share-button"
