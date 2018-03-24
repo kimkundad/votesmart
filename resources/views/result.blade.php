56423c253ca0e9be7c59208f25054d67ea64cbe6
kimkundad <kim.kundad@gmail.com>
2018-03-24 16:59:06 +0700

    golf

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -766,6 +766,8 @@ text-shadow: 1px 1px 0 #5EC8F2, 2px 2px 0 #5EC8F2, 3px 3px 0 #5EC8F2, 4px 4px 0 
           </div>
         </div>
 
+
+
       </div>
      </div>
 
@@ -837,8 +839,12 @@ text-shadow: 1px 1px 0 #5EC8F2, 2px 2px 0 #5EC8F2, 3px 3px 0 #5EC8F2, 4px 4px 0 
                                       success: function(json){
                                           if(json.status == 1000) {
 
+                                            var time = Date.now || function() {
+                                                return +new Date;
+                                              };
 
-                                              window.open('https://www.facebook.com/sharer/sharer.php?u={{url('shared_quiz/'.Auth::user()->id)}}', '_blank');
+
+                                              window.open('https://www.facebook.com/sharer/sharer.php?u={{url('shared_quiz/'.Auth::user()->id)}}?time='+time(), '_blank');
 
 
                                            } else {
