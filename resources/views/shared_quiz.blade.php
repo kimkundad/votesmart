35cf86c67d17e4018deb8d2808237508fa425a0a
kimkundad <kim.kundad@gmail.com>
2018-03-24 21:56:02 +0700

    golf

--- resources/views/shared_quiz.blade.php
+++ resources/views/shared_quiz.blade.php
@@ -18,25 +18,22 @@
 <body>
 
   <!-- Load Facebook SDK for JavaScript -->
-  <div id="fb-root"></div>
-  <script>
-
-  setTimeout(function(){
-    window.location = "{{url('/')}}";
-  }, 900);
-
-  (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "http://connect.facebook.net/en_US/sdk.js#xfbml=1";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
 
 
 
+<div id="fb-root"></div>
+<script>
+setTimeout(function(){
+  window.location = "{{url('/')}}";
+}, 900);
 
-</script>
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/th_TH/sdk.js#xfbml=1&version=v2.12&appId=148045139197033&autoLogAppEvents=1';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 
   <!-- Your share button code -->
   <div class="fb-share-button"
