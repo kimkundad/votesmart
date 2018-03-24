17e84b14e8d5227d5372cad58ef16e73c5595c15
kimkundad <kim.kundad@gmail.com>
2018-03-24 21:34:52 +0700

    golf

--- resources/views/shared_quiz.blade.php
+++ resources/views/shared_quiz.blade.php
@@ -3,13 +3,17 @@
   <title>ถ้าต้องบริหารประเทศไทย</title>
     <!-- You can use Open Graph tags to customize link previews.
     Learn more: https://developers.facebook.com/docs/sharing/webmasters -->
-  <meta property="og:url"           content="http://devzab.com/shared_quiz/{{$user->id}}?v={{time()}}" />
+  <?php
+    $time_fb = time();
+   ?>
+  <meta property="og:url"           content="http://devzab.com/shared_quiz/{{$user->id}}?v={{$time_fb}}" />
   <meta property="og:type"          content="website" />
   <meta property="og:title"         content="ถ้าต้องบริหารประเทศไทย" />
   <meta property="og:description"   content="{{$user->name}} จะเลือกเรื่องเหล่านี้ วิสัยทัศน์ดีแบบนี้ เราเลยสร้างโปสเตอร์หาเสียงให้คุณแล้วล่ะ" />
-
+  <meta property="og:image:width" content="500" />
+  <meta property="og:image:height" content="330" />
   <meta property="fb:app_id" content="148045139197033">
-  <meta property="og:image"         content="http://devzab.com/assets/image/shared/{{$user->image_shared}}?v={{time()}}" />
+  <meta property="og:image"         content="http://devzab.com/assets/image/shared/{{$user->image_shared}}?v={{$time_fb}}" />
 </head>
 <body>
 
@@ -36,7 +40,7 @@
 
   <!-- Your share button code -->
   <div class="fb-share-button"
-    data-href="http://devzab.com/shared_quiz/{{$user->id}}?v={{time()}}"
+    data-href="http://devzab.com/shared_quiz/{{$user->id}}?v={{$time_fb}}"
     data-layout="button_count">
   </div>
 
