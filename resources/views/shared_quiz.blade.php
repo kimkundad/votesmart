48014d712f6dbd5a955819701d20a4eb72709799
kimkundad <kim.kundad@gmail.com>
2018-03-24 21:46:32 +0700

    golf

--- resources/views/shared_quiz.blade.php
+++ resources/views/shared_quiz.blade.php
@@ -10,8 +10,6 @@
   <meta property="og:type"          content="website" />
   <meta property="og:title"         content="ถ้าต้องบริหารประเทศไทย" />
   <meta property="og:description"   content="{{$user->name}} จะเลือกเรื่องเหล่านี้ วิสัยทัศน์ดีแบบนี้ เราเลยสร้างโปสเตอร์หาเสียงให้คุณแล้วล่ะ" />
-  <meta property="og:image:width" content="600" />
-  <meta property="og:image:height" content="315" />
   <meta property="fb:app_id" content="148045139197033">
   <meta property="og:image"         content="http://devzab.com/assets/image/shared/{{$user->image_shared}}?v={{$time_fb}}" />
 </head>
