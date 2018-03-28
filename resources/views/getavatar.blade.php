535ac254d83c6e0ec94670b3799508bcfa4ea798
kimkundad <kim.kundad@gmail.com>
2018-03-28 19:34:01 +0700

    golf

--- resources/views/getavatar.blade.php
+++ resources/views/getavatar.blade.php
@@ -1,1 +1,12 @@
-<img src="//graph.facebook.com/{{$objs->provider_user_id}}/picture?width=300&height=300"  style=" height:350px; width:350px;" >
+<?php
+$image = '//graph.facebook.com/'.$objs->provider_user_id.'/picture?width=300&height=300';
+// Read image path, convert to base64 encoding
+$imageData = base64_encode(file_get_contents($image));
+
+// Format the image SRC:  data:{mime};base64,{data};
+$src = 'data: '.mime_content_type($image).';base64,'.$imageData;
+
+// Echo out a sample image
+echo '<img src="' . $src . '">';
+ ?>
+<!--<img src="//graph.facebook.com/{{$objs->provider_user_id}}/picture?width=300&height=300"  style=" height:350px; width:350px;" >-->
