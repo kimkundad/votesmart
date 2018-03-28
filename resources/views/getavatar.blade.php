a0167f3a0979602b7f8829b28df9157ebc4ec7b0
kimkundad <kim.kundad@gmail.com>
2018-03-28 19:40:59 +0700

    golf

--- resources/views/getavatar.blade.php
+++ resources/views/getavatar.blade.php
@@ -1,12 +1,13 @@
 <?php
-$image = '//graph.facebook.com/'.$objs->provider_user_id.'/picture?width=300&height=300';
+//$image = '//graph.facebook.com/'.$objs->provider_user_id.'/picture?width=300&height=300';
+//$image = '//graph.facebook.com/1556099071134652/picture?width=300&height=300';
 // Read image path, convert to base64 encoding
-$imageData = base64_encode(file_get_contents($image));
+//$imageData = base64_encode($image);
 
 // Format the image SRC:  data:{mime};base64,{data};
-$src = 'data: '.mime_content_type($image).';base64,'.$imageData;
+//$src = 'data: '.$image.';base64,'.$imageData;
 
 // Echo out a sample image
-echo '<img src="' . $src . '">';
+//echo '<img src="' . $src . '">';
  ?>
-<!--<img src="//graph.facebook.com/{{$objs->provider_user_id}}/picture?width=300&height=300"  style=" height:350px; width:350px;" >-->
+<img src="//graph.facebook.com/1556099071134652/picture?width=300&height=300"  style=" height:350px; width:350px;" >
