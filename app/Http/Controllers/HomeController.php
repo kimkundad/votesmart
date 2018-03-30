fd1ed35c154fb8ac60475b82b18371d81d3e2a88
kimkundad <kim.kundad@gmail.com>
2018-03-30 21:06:36 +0700

    golf

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -194,7 +194,8 @@ class HomeController extends Controller
 
       $image_file = $fid.'.jpg';
 
-      $img_save_location = $_SERVER['DOCUMENT_ROOT'].'/assets/image/avatar/'.$image_file;
+      //$img_save_location = $_SERVER['DOCUMENT_ROOT'].'/assets/image/avatar/'.$image_file;
+      $img_save_location = $_SERVER['DOCUMENT_ROOT'].'/votesmart/public/assets/image/avatar/'.$image_file;
       /*Path to the location to save the image on your server*/
 
       //dd(file_get_contents($fb_url));
@@ -220,8 +221,8 @@ class HomeController extends Controller
 
       $image_file = $fid.'.jpg';
 
-      $img_save_location = $_SERVER['DOCUMENT_ROOT'].'/assets/image/avatar/'.$image_file;
-    //  $img_save_location = $_SERVER['DOCUMENT_ROOT'].'/votesmart/public/assets/image/avatar/'.$image_file;
+    //  $img_save_location = $_SERVER['DOCUMENT_ROOT'].'/assets/image/avatar/'.$image_file;
+      $img_save_location = $_SERVER['DOCUMENT_ROOT'].'/votesmart/public/assets/image/avatar/'.$image_file;
       /*Path to the location to save the image on your server*/
 
 
