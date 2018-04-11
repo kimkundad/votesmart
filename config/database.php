b3ebacbd3f3bfc3889a01162f965765c0c3c7364
root <root@folktag.com>
2018-04-11 10:48:17 +0000

    add cleardb

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
 'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'db4free.net'),
+            'host' => env('DB_HOST', 'us-cdbr-iron-east-05.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'votesmart'),
-            'username' => env('DB_USERNAME', 'votesmart'),
-            'password' => env('DB_PASSWORD', 'votesmart1234'),
+            'database' => env('DB_DATABASE', 'heroku_f0d989455534342'),
+            'username' => env('DB_USERNAME', 'b631ae272af226'),
+            'password' => env('DB_PASSWORD', '029be58'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
