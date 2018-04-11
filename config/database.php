b7082a4e7bd60d921ab38608b9a623eef0add0ec
root <root@folktag.com>
2018-04-11 10:44:28 +0000

    add db

--- config/database.php
+++ config/database.php
@@ -39,13 +39,13 @@ return [
             'prefix' => '',
         ],
 
-        'mysql' => [
+'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'votesmart'),
+            'username' => env('DB_USERNAME', 'votesmart'),
+            'password' => env('DB_PASSWORD', 'votesmart1234'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
