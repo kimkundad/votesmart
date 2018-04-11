5d10e2b31b8b26ed0f17234497ff6090d448ecd6
root <root@folktag.com>
2018-04-11 11:05:45 +0000

    if no then do other thing

--- config/database.php
+++ config/database.php
@@ -1,5 +1,12 @@
 <?php
 
+$url = parse_url(env("CLEARDB_DATABASE_URL"));
+
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
+
 return [
 
     /*
@@ -41,11 +48,11 @@ return [
 
 'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'us-cdbr-iron-east-05.cleardb.net'),
+            'host' => $host,
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'heroku_f0d989455534342'),
-            'username' => env('DB_USERNAME', 'b631ae272af226'),
-            'password' => env('DB_PASSWORD', '029be58'),
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
