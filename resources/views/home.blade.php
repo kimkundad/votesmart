7c1a8bd80be1923aefed5edf93f269d491c32305
kimkundad <kim.kundad@gmail.com>
2018-04-20 14:30:03 +0700

    golf

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -158,12 +158,72 @@
 
 <style>
 
+.animate {
+  transition: transform 0.3s ease-out;
+}
+.slider-wrap {
+
+  position: absolute;
+
+}
+.slider {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+.ms-touch.slider {
+  overflow-x: scroll;
+  overflow-y: hidden;
+
+  -ms-overflow-style: none;
+  /* Hides the scrollbar. */
+
+  -ms-scroll-chaining: none;
+  /* Prevents Metro from swiping to the next tab or app. */
+
+  -ms-scroll-snap-type: mandatory;
+  /* Forces a snap scroll behavior on your images. */
+
+  -ms-scroll-snap-points-x: snapInterval(0%, 100%);
+  /* Defines the y and x intervals to snap to when scrolling. */
+}
+.holder {
+  width: 33.333%;
+  height: 100%;
+  float: left;
+  height: 500px;
+  position: relative;
+  overflow: hidden;
+}
+
+.slide-wrapper {
+
+  height: 100%;
+  float: left;
 
 
 
+}
+.slide {
+  height: 100%;
 
+}
+.slide .mask-content {
 
+  z-index: 0;
+  transform: translatex(-100px);
 
+}
+.slide:before {
+  content: "";
+  position: absolute;
+  z-index: 1;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 40%;
+
+}
 </style>
 
 
@@ -172,11 +232,12 @@
 
 
 
-<div class="content_leena panel-content" style="position: fixed;">
+<div class="slider-wrap content_leena panel-content" style="position: fixed;">
+<div class="slider" id="slider">
+  <div class="holder">
 
-
-  <div id="content-home1" class="content-home">
-    <div class="real-content" style="padding-top: 70px;">
+  <div id="content-home1" class="content-home slide-wrapper">
+    <div class="slide real-content" style="padding-top: 70px;">
       <div class="mask-content">
         <!-- First Container -->
         <div class="welcome-section text-center">
@@ -207,8 +268,8 @@
     </div>
   </div>
 
-  <div id="content-home2" class="content-home">
-    <div class="real-content" style="padding-top: 70px;">
+  <div id="content-home2" class="content-home slide-wrapper">
+    <div class="slide real-content" style="padding-top: 70px;">
 
 
       <div class="mask-content">
@@ -240,6 +301,9 @@
       </div>
     </div>
   </div>
+
+</div>
+</div>
 </div>
 
 @else
@@ -362,11 +426,117 @@
 <script>
 
 
-$(function() {
+$(document).ready(function () {
   $('#content-home1').show();
-      
+
+
+
+
+    $(".hometo1").click(function(){
+        $(".content_leena").slideUp();
+  });
+
+
+
+
+  if (navigator.msMaxTouchPoints) {
+
+  $('#slider').addClass('ms-touch');
+
+  $('#slider').on('scroll', function() {
+    $('.slide-image').css('transform','translate3d(-' + (100-$(this).scrollLeft()/6) + 'px,0,0)');
+  });
+
+} else {
+
+  var slider = {
+
+    el: {
+      slider: $("#slider"),
+      holder: $(".holder"),
+      imgSlide: $(".slide-image")
+    },
+
+    slideWidth: $('#slider').width(),
+    touchstartx: undefined,
+    touchmovex: undefined,
+    movex: undefined,
+    index: 0,
+    longTouch: undefined,
+
+    init: function() {
+      this.bindUIEvents();
+    },
+
+    bindUIEvents: function() {
+
+      this.el.holder.on("touchstart", function(event) {
+        slider.start(event);
+      });
+
+      this.el.holder.on("touchmove", function(event) {
+        slider.move(event);
+      });
+
+      this.el.holder.on("touchend", function(event) {
+        slider.end(event);
+      });
+
+    },
+
+    start: function(event) {
+      // Test for flick.
+      this.longTouch = false;
+      setTimeout(function() {
+        window.slider.longTouch = true;
+      }, 250);
+
+      // Get the original touch position.
+      this.touchstartx =  event.originalEvent.touches[0].pageX;
+
+      // The movement gets all janky if there's a transition on the elements.
+      $('.animate').removeClass('animate');
+    },
+
+    move: function(event) {
+      // Continuously return touch position.
+      this.touchmovex =  event.originalEvent.touches[0].pageX;
+      // Calculate distance to translate holder.
+      this.movex = this.index*this.slideWidth + (this.touchstartx - this.touchmovex);
+      // Defines the speed the images should move at.
+      var panx = 100-this.movex/6;
+      if (this.movex < 600) { // Makes the holder stop moving when there is no more content.
+        this.el.holder.css('transform','translate3d(-' + this.movex + 'px,0,0)');
+      }
+      if (panx < 100) { // Corrects an edge-case problem where the background image moves without the container moving.
+        this.el.imgSlide.css('transform','translate3d(-' + panx + 'px,0,0)');
+      }
+    },
+
+    end: function(event) {
+      // Calculate the distance swiped.
+      var absMove = Math.abs(this.index*this.slideWidth - this.movex);
+      // Calculate the index. All other calculations are based on the index.
+      if (absMove > this.slideWidth/2 || this.longTouch === false) {
+        if (this.movex > this.index*this.slideWidth && this.index < 2) {
+          this.index++;
+        } else if (this.movex < this.index*this.slideWidth && this.index > 0) {
+          this.index--;
+        }
+      }
+      // Move and animate the elements.
+      this.el.holder.addClass('animate').css('transform', 'translate3d(-' + this.index*this.slideWidth + 'px,0,0)');
+      this.el.imgSlide.addClass('animate').css('transform', 'translate3d(-' + 100-this.index*50 + 'px,0,0)');
+
     }
-);
+
+  };
+
+  slider.init();
+}
+
+
+});
 
 
   /*$(document).ready(function () {
