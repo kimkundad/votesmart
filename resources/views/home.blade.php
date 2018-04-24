572de5545944ee56faf931d24f7a32726a1f0c5e
kimkundad <kim.kundad@gmail.com>
2018-04-24 21:57:49 +0700

    golf

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -39,7 +39,7 @@
   </div>
 </section>
 
-<div class="toggle toggle-primary toggle-sm" style="z-index: 1;">
+<div class="toggle toggle-primary toggle-sm" style="position: absolute; z-index: 1000;">
   <div class="toggle active zoom">
     <label id="zoomBtn" class="zoom-fab zoom-btn-large"
     style="border-radius: 5px 5px 5px 5px; padding: 10px; text-align: left; box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -2px rgba(0, 0, 0, 0.2);">
