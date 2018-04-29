7980fce8237b7fd431a95fe12cc7a36d8ec4756b
kimkundad <kim.kundad@gmail.com>
2018-04-29 22:43:53 +0700

    golf

--- resources/views/reps_detail.blade.php
+++ resources/views/reps_detail.blade.php
@@ -1903,50 +1903,8 @@ return "$strDay $strMonthThai";
 <script>
 
 
-$(document).ready(function () {
 
 
-
-
-
-    $(document).on("scroll", onScroll);
-
-    //smoothscroll
-    $('a[href^="#"]').on('click', function (e) {
-        e.preventDefault();
-        $(document).off("scroll");
-
-        $('a').each(function () {
-            $(this).removeClass('active');
-        })
-        $(this).addClass('active');
-
-        var target = this.hash,
-            menu = target;
-        $target = $(target);
-        $('html, body').stop().animate({
-            'scrollTop': $target.offset().top+2
-        }, 500, 'swing', function () {
-            window.location.hash = target;
-            $(document).on("scroll", onScroll);
-        });
-    });
-});
-
-function onScroll(event){
-    var scrollPos = $(document).scrollTop();
-    $('#menu-center a').each(function () {
-        var currLink = $(this);
-        var refElement = $(currLink.attr("href"));
-        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-            $('#menu-center ul li a').removeClass("active");
-            currLink.addClass("active");
-        }
-        else{
-            currLink.removeClass("active");
-        }
-    });
-}
 
 
 $(document).ready(function(){
@@ -2058,7 +2016,7 @@ if (iconRotate.length != 0) {
 		iconRotate.css({transform: 'rotate(-' + (360 * scroll/maxScroll) + 'deg)'});
     var set = 0.7;
     var set_num = (360 * scroll/maxScroll);
-    console.log($(document).height());
+
 
 
 
@@ -2103,7 +2061,7 @@ if (iconRotate.length != 0) {
         // set the width
         $('.img-in-chart').css('width', zoomedWidth);
         $('.img-in-chart').css('height', zoomedWidth);
-        console.log(set_num);
+
 
         if(zoomedWidth == 247){
           $('.img-in-chart').css('top', 85);
