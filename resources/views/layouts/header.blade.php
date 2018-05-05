f431f9b30fe5f592d50bd685f3e0722f28a5296b
kimkundad <kim.kundad@gmail.com>
2018-05-05 12:20:37 +0700

    golfd

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -264,7 +264,7 @@
         </li>
 
         <li class="nav-item hidden-sm hidden-xs">
-          <a class="btn btn-light btn-asa btn-xl js-scroll-trigger " href="#" data-toggle="modal" data-target="#myModal"
+          <a class="btn btn-light btn-asa btn-xl js-scroll-trigger " style="margin-right: 120px;" href="#" data-toggle="modal" data-target="#myModal"
           style="background-color: #ffffff; font-size: 14px; margin-top: 5px; color:#08B0ED">
           <i class="fa fa-hand-paper-o"></i> อาสาช่วยงาน</a>
         </li>
@@ -382,7 +382,7 @@ figure {
         @if (Auth::guest())
 
         @else
-        <div id="dd" class="wrapper-dropdown-3" tabindex="1" style="padding: 0px;">
+        <div id="dd" class="wrapper-dropdown-3" tabindex="1" style="    top: -25px; padding: 0px;">
 
                         @if(Auth::user()->provider == 'email')
 
