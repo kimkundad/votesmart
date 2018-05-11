7515a3bc1f0815dd8d71880be23d7e790497171b
kimkundad <kim.kundad@gmail.com>
2018-05-11 11:04:30 +0700

    golf

---
+++ resources/views/home_page.blade.php
@@ -0,0 +1,501 @@
+<html lang="en"><head><meta name="csrf-token" content="BPcQ4txNDZSqkgX3oizdHeVpwHE03a78gEnGwsJ4">
+<link href="https://votesmart.me/swiper-4.2.2/dist/css/swiper.css" rel="stylesheet">
+
+
+
+
+
+
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Votesmart - โหวตได้โหวตดี</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="front/vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="front/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- Plugin CSS -->
+    <link href="front/vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+    <link href="//fonts.googleapis.com/css?family=Kanit:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="{{url('front/css/creative.css')}}" rel="stylesheet">
+    <link href="{{url('front/css/css/style.css')}}" rel="stylesheet">
+    <link href="//fonts.googleapis.com/css?family=Kanit" rel="stylesheet">
+<style>
+    .the-logo {
+        margin-bottom: 30px;
+    }
+    body{
+        background: linear-gradient(270deg, #08B0ED 0%, #0479BD 100%) !important;
+        font-family: 'Kanit', sans-serif;
+    }
+    .text-center{
+        text-align: center;
+    }
+    #mainNav .navbar-brand {
+        color: #fff;
+    }
+    .navbar-light .navbar-nav .nav-link{
+        color: #fff !important;
+    }
+    .navbar-nav{
+        margin: 0 auto;
+    }
+    .navbar-nav li a{
+        font-size: 18px;
+        font-weight: 500;
+        letter-spacing: 0.2px;
+        line-height: 27px;
+        text-align: center;
+    }
+    .navbar-nav li.nav-item-3{
+        position: absolute;
+        right: 0;
+    }
+    .navbar-nav li.nav-item-3 a{
+        margin: 0px 25px 0 0 !important;
+        background: transparent;
+        border: 1px solid #FFFFFF;
+        border-radius: 24px;
+        color: #fff;
+        font-size: 14px;
+        line-height: 21px;
+        text-align: center;
+        text-shadow: 0 1px 2px rgba(35,31,32,0.24);
+    }
+    .section-one h1{
+        font-size: 24px;
+        letter-spacing: 0.18px;
+        line-height: 35px;
+        color: #fff;
+        /* max-width: 241px; */
+       margin-bottom: 50px;
+
+    }
+    .section-one-left{
+        height: 500px;
+        vertical-align: middle;
+        display: table-cell;
+        padding-left: 45%;
+    }
+    .section-one{
+        min-height: 600px;
+    }
+    .section-two{
+        background: #f2f8fa;
+        padding: 100px;
+    }
+    .cta{
+        border-radius: 8px;
+        background-color: #FFFFFF;
+        box-shadow: 0 2px 12px  rgba(0,0,0,0.06);
+        padding: 25px;
+    }
+    .cta h3{
+        color: #0479BD;
+        font-size: 36px;
+        font-weight: 500;
+        letter-spacing: 0.27px;
+        line-height: 54px;
+        text-align: center;
+        margin-top: 20px;
+        margin-bottom: 20px;
+    }
+    .cta p{
+        color: #4A4A4A;
+        font-size: 20px;
+        font-weight: 300;
+        letter-spacing: 0.15px;
+        line-height: 30px;
+        text-align: center;
+        max-width: 185px;
+        margin: auto;
+        margin-bottom: 50px;
+    }
+    .cta a{
+        color: #08B0ED;
+        font-size: 16px;
+        line-height: 16px;
+        text-align: center;
+    }
+    .text-h2 h2{
+        color: #0479BD;
+        font-size: 56px;
+        font-weight: 500;
+        letter-spacing: 0.42px;
+        line-height: 80px;
+        text-shadow: 1px 1px 0 #5EC8F2, 2px 2px 0 #5EC8F2, 3px 3px 0  #5EC8F2, 4px 4px 0 #5EC8F2, 5px 5px 0 #5EC8F2, 6px 6px 0 #5EC8F2, 7px 7px 0 #5EC8F2, 8px 8px 0 #5EC8F2, 9px 9px 0 #5EC8F2, 10px 10px 0 #5EC8F2, 11px 11px 0 #5EC8F2, 12px 12px 0 #5EC8F2, 13px 13px 0 #5EC8F2, 14px 14px 0 #5EC8F2, 15px 15px 0 #5EC8F2;
+    }
+    .text-p{
+        color: #4A4A4A;
+        font-size: 20px;
+        font-weight: 300;
+        letter-spacing: 0.15px;
+        line-height: 30px;
+    }
+    .section-footer{
+        padding: 45px 100px;;
+    }
+    .section-footer p{
+        color: #fff;
+        font-size: 14px;
+        letter-spacing: 0.11px;
+        line-height: 21px;
+        text-align: right;
+    }
+    .btn-lp:hover {
+        color:#fff;
+    }
+    .btn-lp{
+        color: #FFFFFF;
+        font-size: 24px;
+        line-height: 35px;
+        text-align: center;
+        padding: 10px 75px;
+        border-radius: 32px;
+        background-color: #08B0ED;
+    }
+    @media only screen and (max-width: 1024px) {
+        .section-one-left{
+            padding-left: 25%;
+        }
+        .section-two{
+            padding:100px 50px;
+        }
+        .cta p{
+            font-size: 18px;
+        }
+        .cta h3{
+            margin-bottom: 20px;
+        }
+        .section-footer{
+            padding: 45px 50px;
+        }
+        .section-one-left{
+            height: 400px;
+        }
+
+    }
+    @media only screen and (max-width:768px){
+        .section-one .col-md-8 img{
+            height: 400px !important;
+        }
+        .section-one-left{
+            padding-left: 0;
+        }
+        .section-one{
+            min-height: 400px;
+        }
+        .navbar-light .navbar-toggler{
+            border:none;
+        }
+        #mainNav{
+            border-bottom:none;
+        }
+        .cta p{
+            font-size: 14px;
+        }
+        .text-h2 h2{
+            font-size: 40px;
+            line-height: 60px;
+            margin-bottom: 20px;
+        }
+        .text-p{
+            font-size: 16px;
+            line-height: 24px;
+        }
+    }
+    @media only screen and (max-width:767px){
+        .section-one{
+            min-height: 480px;
+        }
+        .cta{
+            margin-bottom: 30px;
+        }
+        .section-one h1{
+            text-align: center;
+        }
+        .btn-lp{
+            text-align: center;
+            display: block;
+            margin: auto;
+            max-width: 245px
+        }
+        .section-one-left{
+            height: 300px;
+        }
+        .section-footer p{
+            text-align: center;
+        }
+        .text-h2{
+            text-align: center;
+            margin-bottom: 30p;
+        }
+        .cta p{
+            font-size: 18px;
+        }
+        .section-one .col-md-8 img{
+            height: 350px !important;
+        }
+    }
+</style>
+</head><body><nav class="navbar navbar-expand-lg navbar-light page-header" id="mainNav" style="background-color: transparent; padding-top:5px; padding-bottom:5px;">
+  <div class="container-fluid">
+
+
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand " href="{{url('home')}}" style="font-size: 1.8rem; font-weight: 700;"><i>เลือกได้เลือกดี</i></a>
+
+            <!-- Modal -->
+            <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+              <div class="modal-dialog" role="document">
+                <div class="modal-content">
+
+                  <div class="modal-body text-center">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close" style="color:#fff; padding: 0px 0px 10px 10px;"><span aria-hidden="true">×</span> ปิด</button>
+
+                    <br><br>
+                    <h3 class="text-center">อาสา</h3>
+                    <p class="p-pop">ร่วมกันสร้างความเปลี่ยนแปลง</p>
+                    <hr class="my-4" style="margin: 0 auto;">
+                    <p class="p-pop" id="text2">หากคุณเป็นคนๆนึงที่อยากเห็นความเปลี่ยนแปลง <br>
+                    ไม่ว่าคุณจะอายุเท่าไหร่ เพศไหน<br>
+                    หรือมีความถนัดอะไร<br>
+                    เราต้องการคุณมาร่วมเปลี่ยนแปลงไปด้วยกัน</p>
+
+                    <p class="p-pop" id="text2">เพียงกรอกข้อมูลด้านล่าง<br>
+                    ทีมงานของเราจะรีบติดต่อกลับไปโดยเร็ว</p>
+
+                    <h3 id="head1"></h3>
+                    <h3 id="head2" style="color:red"></h3>
+
+                    <form action="{{url('contact')}}" id="form" method="post" enctype="multipart/form-data">
+                    {{ csrf_field() }}
+                    <div class="col-md-12 " style="padding-right: 0px; padding-left: 0px;">
+
+                          <div class="form-group">
+                            <label for="exampleInputEmail1" style="pull-left">ชื่อ</label>
+                            <input type="text" name="name" id="name" class="form-control">
+                          </div>
+
+                        </div>
+                    <div class="col-md-12 " style="padding-right: 0px; padding-left: 0px;">
+                      <div class="form-group">
+                        <label for="exampleInputEmail1">สกุล</label>
+                        <input type="text" name="surname" id="surname" class="form-control">
+                      </div>
+                    </div>
+
+                    <div class="col-md-12 " style="padding-right: 0px; padding-left: 0px;">
+                      <div class="form-group">
+                        <label for="exampleInputEmail1">อีเมล</label>
+                        <input type="email" name="email" id="email" class="form-control">
+                      </div>
+                    </div>
+
+                    <div class="col-md-12 " style="padding-right: 0px; padding-left: 0px;">
+                      <div class="form-group">
+                        <label for="exampleInputEmail1">อายุ</label>
+                        <input type="number" name="year_old" id="year_old" class="form-control">
+                      </div>
+                    </div>
+
+                    <div class="col-md-12 " style="padding-right: 0px; padding-left: 0px;">
+                      <div class="form-group" style="height:55px;">
+
+                        <label for="exampleInputEmail1">เพศ</label>
+                        <br>
+                    				<div class="form-check">
+                    					<label>
+                    						<input type="radio" name="radio" id="radio" value="ชาย"> <span class="label-text" style="margin-right: 15px;">ชาย</span>
+                    					</label>
+                    				</div>
+
+                    				<div class="form-check">
+                    					<label>
+                    						<input type="radio" name="radio" id="radio" value="หญิง"> <span class="label-text" style="margin-right: 15px;">หญิง</span>
+                    					</label>
+                    				</div>
+                    				<div class="form-check">
+                    					<label>
+                    						<input type="radio" name="radio" id="radio" value="ไม่ระบุ"> <span class="label-text" style="margin-right: 15px;">ไม่ระบุ</span>
+                    					</label>
+                    				</div>
+
+                      </div>
+                    </div>
+
+
+                    <div class="col-md-12 " style="padding-right: 0px; padding-left: 0px;">
+                      <div class="form-group">
+                        <label for="exampleInputEmail1">ความสนใจ</label>
+                        <textarea class="form-control" name="detail" rows="3" id="detail"></textarea>
+                      </div>
+                    </div>
+
+                    <div class="text-center">
+                      <button type="submit" class="btn btn-light btn-block" style="border-radius: 3px; color: #08B0ED;">ยืนยัน</button>
+                    </div>
+                  </form>
+
+
+
+
+
+                  </div>
+
+                </div>
+              </div>
+            </div>
+
+
+
+
+
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item action-nav">
+          <a class="nav-link a-head   js-scroll-trigger hidden-sm hidden-xs" id="&quot;a-head-set&quot;" style="font-size: 16px; padding: 16px 25px 10px; " href="{{url('home')}}">เลือกอะไรได้?</a>
+
+        </li>
+        <li class="nav-item  " style="margin-right: 30px;">
+
+          <a class="nav-link    js-scroll-trigger hidden-sm hidden-xs" style="font-size: 16px; padding: 16px 25px 10px; " href="{{url('representatives_all')}}">เลือกใครดี?</a>
+
+        </li>
+
+        <li class="nav-item nav-item-3 hidden-sm hidden-xs">
+          <a class="btn btn-light btn-asa btn-xl js-scroll-trigger " style="margin-right: 120px; margin-top: 8px;" href="#" data-toggle="modal" data-target="#myModal">
+          <i class="fa fa-hand-paper-o"></i> อาสาช่วยงาน</a>
+          </li>
+
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+
+
+
+<section id="services" style=" padding: 1.5rem 0 0;">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="section-one  col-md-12">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="section-one-left" style="    text-align: center;">
+                                    <img src="{{url('assets/image/votesmart-logo.svg')}}" class="img-fluid the-logo">
+                                    <h1>
+                                    <p>เปิดพื้นที่ให้ทุกคนได้เลือก
+                                            ว่าอะไรที่ดีสำหรับคุณ
+                                            และประเทศของเรา </p>
+                                    </h1>
+                                    <a href="/redirect" class="btn-lp">เข้าร่วม</a>
+                                </div>
+                            </div>
+                            <div class="col-md-8">
+                                <div class="text-p">
+                                    <img style="width: auto; height: 600px;float: right;position: absolute;right: 0;" src="{{url('assets/image/vote-by-desktop@2x.png')}}">
+                                </div>
+                            </div>
+                            </div>
+                    </div>
+                 <div class="section-two col-md-12" style="background: #f2f8fa;">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="cta text-center">
+                                <img src="{{url('assets/image/pie.svg')}}" class="the-icon">
+                                <h3>เลือกได้</h3>
+                                <p>ร่วมกันแสดงออกว่า เราเลือกอะไร</p>
+                                <a href="/redirect">เข้าร่วม  &gt;</a>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="cta text-center">
+                                    <img src="{{url('assets/image/id.svg')}}" class="the-icon">
+                                <h3>เลือกดี</h3>
+                                <p>จะเลือกใคร..เลือกให้ดี?
+                                        รู้จักเค้าคนนั้นดีหรือยัง</p>
+                                <a href="/representatives_all">ดูข้อมูลผู้สมัคร &gt;</a>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="cta text-center">
+                                    <img src="{{url('assets/image/paper.svg')}}" class="the-icon">
+                                <h3>อาสา</h3>
+                                <p>ยินดีเสมอถ้าจะมีคน
+                                        มาช่วยกันเปลี่ยนแปลง</p>
+                                <a href="#">อาสาช่วยงาน &gt;</a>
+                            </div>
+                        </div>
+                     </div>
+                </div>
+                <div class="section-two col-md-12" style="background: #FFFFFF;">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="text-h2">
+                                <h2>ทำไมต้อง</h2>
+                                <h2>
+                                    เลือกได้ เลือกดี?
+                                </h2>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="text-p per">
+                                <p>
+                                    There are many kinds of narratives and organizing principles. Science is driven by evidence gathered in experiments, and by the falsification of extant theories and their replacement with newer, asymptotically truer, ones. Other systems – religion, nationalism, paranoid ideation, or art – are based on personal experiences (faith, inspiration, paranoia, etc.).
+                                </p>
+                            </div>
+                        </div>
+                     </div>
+                </div>
+            <div class="section-footer col-md-12" style="background: #08B0ED;">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="text-h2">
+                            <a style="color:#fff;" class="navbar-brand " href="{{url('home')}}">
+                                <img src="{{url('assets/image/votesmart-logo.svg')}}" class="img-fluid the-logo" style="width: 180px;">
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="text-p">
+                            <p>© 2018 VOTESMART</p>
+                        </div>
+                    </div>
+                    </div>
+            </div>
+            </div>
+    </div>
+</section>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="https://votesmart.me/front/vendor/jquery/jquery.min.js"></script>
+    <script src="https://votesmart.me/front/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="https://votesmart.me/front/vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="https://votesmart.me/front/vendor/scrollreveal/scrollreveal.min.js"></script>
+    <script src="https://votesmart.me/front/vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+
+    <!-- Custom scripts for this template -->
+    <script src="https://votesmart.me/front/js/creative.js"></script>
+
+
+
+
+
+
+</body></html>
