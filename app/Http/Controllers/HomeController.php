0a7006859cf1fb5d6e88d0d0a411c6064dc874b4
kimkundad <kim.kundad@gmail.com>
2018-05-21 10:20:11 +0700

    golf

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -105,7 +105,7 @@ class HomeController extends Controller
 
         return view('home')->with([
          'cat' => $cat,
-         compact('objs')
+         'objs' => compact('objs')
        ]);
     }
 
