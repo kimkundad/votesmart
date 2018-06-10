5cdeb9f17309d6a0dbd532aa6640b6a5ad9fa190
kimkundad <kim.kundad@gmail.com>
2018-06-11 06:59:16 +0700

    vote

--- resources/views/representatives_grid.blade.php
+++ resources/views/representatives_grid.blade.php
@@ -166,7 +166,7 @@ input:-webkit-autofill {
                       <select name="cars" class="fieldf-select2  align-right" style="width:73%" onchange="this.form.submit()">
                         @if($objs_pro)
                            @foreach($objs_pro as $pro)
-                          <option value="{{$pro->id_p}}" <?php if ($pro->id_p == $cars ) echo 'selected' ; ?> >ผู้สมัคร ส.ส. {{$pro->name_in_thai1}}</option>
+                          <option value="{{$pro->id_p}}" >ผู้สมัคร ส.ส. {{$pro->name_in_thai1}}</option>
                           @endforeach
                       @endif
                       </select>
